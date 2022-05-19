8eb4ea886c578fe3d9588a479eda4842e4df9693
Simone Trita <simonetrita@gmail.com>
2022-05-19 08:32:52 +0200

    feat(add styledComp): add styled components

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,67 +1,60 @@
-import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React, { useState } from "react";
+
+import { useSelector, useDispatch } from "react-redux";
 import {
   decrement,
   increment,
   incrementByAmount,
   incrementAsync,
   incrementIfOdd,
+  reset,
   selectCount,
-} from './counterSlice';
-import styles from './Counter.module.css';
+} from "./counterSlice";
+// import styles from "./Counter.module.css";
+import { ButtonWrapper, CustomButton, DisplayCounter, Input } from "./styled";
 
 export function Counter() {
   const count = useSelector(selectCount);
   const dispatch = useDispatch();
-  const [incrementAmount, setIncrementAmount] = useState('2');
+  const [incrementAmount, setIncrementAmount] = useState("3");
 
   const incrementValue = Number(incrementAmount) || 0;
 
   return (
-    <div>
-      <div className={styles.row}>
-        <button
-          className={styles.button}
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
-          -
-        </button>
-        <span className={styles.value}>{count}</span>
-        <button
-          className={styles.button}
-          aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-        >
-          +
-        </button>
-      </div>
-      <div className={styles.row}>
-        <input
-          className={styles.textbox}
-          aria-label="Set increment amount"
-          value={incrementAmount}
-          onChange={(e) => setIncrementAmount(e.target.value)}
-        />
-        <button
-          className={styles.button}
-          onClick={() => dispatch(incrementByAmount(incrementValue))}
-        >
-          Add Amount
-        </button>
-        <button
-          className={styles.asyncButton}
-          onClick={() => dispatch(incrementAsync(incrementValue))}
-        >
-          Add Async
-        </button>
-        <button
-          className={styles.button}
-          onClick={() => dispatch(incrementIfOdd(incrementValue))}
-        >
-          Add If Odd
-        </button>
-      </div>
-    </div>
+    <>
+      <ButtonWrapper>
+        <CustomButton onClick={() => dispatch(decrement())}> - </CustomButton>
+        <CustomButton onClick={() => dispatch(increment())}> + </CustomButton>
+        <DisplayCounter>{count}</DisplayCounter>
+        <CustomButton onClick={() => dispatch(reset())}> Reset </CustomButton>
+
+        <ButtonWrapper>
+          <Input
+            aria-label="Set increment amount"
+            value={incrementAmount}
+            onChange={(e) => setIncrementAmount(e.target.value)}
+          ></Input>
+          <CustomButton
+            onClick={() => dispatch(incrementByAmount(incrementValue))}
+          >
+            {" "}
+            Add Amount
+          </CustomButton>
+
+          <CustomButton
+            onClick={() => dispatch(incrementAsync(incrementValue))}
+          >
+            {" "}
+            Add Async
+          </CustomButton>
+          <CustomButton
+            onClick={() => dispatch(incrementIfOdd(incrementValue))}
+          >
+            {" "}
+            Add if Odd
+          </CustomButton>
+        </ButtonWrapper>
+      </ButtonWrapper>
+    </>
   );
 }
