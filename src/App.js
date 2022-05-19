a0d2c2c177858adb30ef6957f043f835bac698ad
Simone Trita <simonetrita@gmail.com>
2022-05-19 11:36:23 +0200

    feat(add browserRouter): add BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React from "react";
-// import logo from "./logo.svg";
-// import { Counter } from "./features/counter/Counter";
+import logo from "./logo.svg";
+import { Counter } from "./features/counter/Counter";
 import About from "./components/About";
 import Home from "./components/Home";
+import Contacts from "./components/Contacts";
 import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Navbar />
-        <Home />
-        <About />
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <Counter /> */}
-      </header>
-    </div>
+    <>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Home />
+          <About />
+          <Contacts />
+        </div>
+      </Router>
+    </>
   );
 }
 
 export default App;
+
+/*
+BrowserRouter :  andremo a mettere gli elementi che vogllio facciano parte della navigazione all'interno di 
+                 BrowserRouter
+*/
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,14 @@
 import React from "react";
+import "../App.css";
 
 function Navbar() {
   return (
-    <div>
-      Navbar
-      <ul>
+    <div className="navbar">
+      <h2> Routing </h2>
+      <ul className="links">
         <li>Home</li>
         <li>About</li>
+        <li>Contacts</li>
       </ul>
     </div>
   );
